b96ed8688c88cdf3d61f0c273e1856cada64c3f8
João Paulo M. Silva <131725903+JeiPintoul@users.noreply.github.com>
2026-03-29 23:09:57 -0300

    feat: implement GameEngine and event handling for player actions

--- core.io/src/shared/EventBus.ts
+++ core.io/src/shared/EventBus.ts
@@ -12,5 +12,7 @@ export const GameEvents = {
     PLAYER_INPUT: 'player_input', // Quando o dev de UI captura o teclado
     STATE_UPDATE: 'state_update', // Quando a Lógica atualiza as coordenadas
     LEVEL_UP: 'level_up',         // Quando o inimigo morre e o jogador passa de nível
-    GAME_OVER: 'game_over'        // Quando a vida chega a zero, etc
+    GAME_OVER: 'game_over',        // Quando a vida chega a zero, etc
+    ENTITY_DAMAGE: 'entity_damage',
+    ENTITY_DESTROYED: 'entity_destroyed'
 };
